f72996669a5fa4dee6647740ebd2cb3af5722046
lvaillan <classvaillan@gmail.com>
2021-07-18 18:49:49 -0500

    Added time blocks and fixed issues with style

---
+++ assets/js/javascript.js
@@ -0,0 +1,26 @@
+// Set the Scheduler to the current day
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+
+// setInterval(function() {
+//     var descriptionArea = document.querySelectorAll("textarea");
+//     var currentHour = moment().format("H");
+//     console.log(currentHour);
+
+//     descriptionArea.forEach.(function(textarea) {
+//         var hour = textarea.getAttribute("data-hour");
+//         console.log(hour);
+//         if (hour < currentHour) {
+//             textarea.removeClass("future");
+//             textarea.addClass("past");
+//         } 
+//         else if (hour === currentHour) {
+//             textarea.removeClass("future");
+//             textarea.addClass("present");
+//         }
+//     });
+
+// }, 60 * 1000);
+
+var myDay = [
+    
+]
