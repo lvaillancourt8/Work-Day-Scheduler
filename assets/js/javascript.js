c8f74ff33f3d289ca1d269e0c94adc35df7c3d12
lvaillan <classvaillan@gmail.com>
2021-07-18 19:55:52 -0500

    local storage, time block colors and save button listener

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,26 +1,47 @@
 // Set the Scheduler to the current day
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
-// setInterval(function() {
-//     var descriptionArea = document.querySelectorAll("textarea");
-//     var currentHour = moment().format("H");
-//     console.log(currentHour);
+function setTimeBlockColor() {
+    setInterval(function() {
+        var currentHour = moment().format("H");
+        console.log(currentHour);
 
-//     descriptionArea.forEach.(function(textarea) {
-//         var hour = textarea.getAttribute("data-hour");
-//         console.log(hour);
-//         if (hour < currentHour) {
-//             textarea.removeClass("future");
-//             textarea.addClass("past");
-//         } 
-//         else if (hour === currentHour) {
-//             textarea.removeClass("future");
-//             textarea.addClass("present");
-//         }
-//     });
+        $(".description").each(function(textarea) {
+            var hour = $(this).data("hour");
+            console.log(hour);
+            if (hour < currentHour) {
+                $(this).removeClass("future");
+                $(this).addClass("past");
+            } 
+            else if (hour === currentHour) {
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+        });
 
-// }, 60 * 1000);
+    }, 60 * 1000);
+}
 
-var myDay = [
-    
-]
+
+$(".saveBtn").on("click", function () {
+    // Get nearby values of the description in JQuery
+    var text = $(this).siblings(".description").val();
+    var time = $(this).prev().attr("id");
+
+    // Save text in local storage
+    localStorage.setItem(time, text);
+})
+
+// Get saved appointments from local storage
+$("#hour8").text(localStorage.getItem("hour8"));
+$("#hour9").text(localStorage.getItem("hour9"));
+$("#hour10").text(localStorage.getItem("hour10"));
+$("#hour11").text(localStorage.getItem("hour11"));
+$("#hour12").text(localStorage.getItem("hour12"));
+$("#hour13").text(localStorage.getItem("hour13"));
+$("#hour14").text(localStorage.getItem("hour14"));
+$("#hour15").text(localStorage.getItem("hour15"));
+$("#hour16").text(localStorage.getItem("hour16"));
+$("#hour17").text(localStorage.getItem("hour17"));
+
+setTimeBlockColor();
