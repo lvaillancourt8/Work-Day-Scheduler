4b4e9dd7d2fdcf649f9e8f04f7c7e264660680e8
lvaillan <classvaillan@gmail.com>
2021-07-20 10:07:17 -0500

    fixed bugs with time block colors and updated readme file

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,28 +1,59 @@
 // Set the Scheduler to the current day
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
+// Declare a variable for the current hour to use to update the time block color
+var currentHour = moment().format("H");
+
+// Initial scan of the time blocks to set the correct color for past, present and future based on current time
+function initialTimeBlockColor() {
+    currentHour = moment().format("H");
+    $(".description").each(function(textarea) {
+        var hour = $(this).data("hour");
+        if (hour < currentHour) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        } 
+        else if (hour == currentHour) {
+            $(this).removeClass("future");
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        }
+        else if (hour > currentHour) {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    });
+}
+
+// Set an interval to evaluate the current time every minute and update the time block color as needed
 function setTimeBlockColor() {
     setInterval(function() {
-        var currentHour = moment().format("H");
-        console.log(currentHour);
-
+        currentHour = moment().format("H");
         $(".description").each(function(textarea) {
             var hour = $(this).data("hour");
-            console.log(hour);
             if (hour < currentHour) {
                 $(this).removeClass("future");
+                $(this).removeClass("present");
                 $(this).addClass("past");
             } 
-            else if (hour === currentHour) {
+            else if (hour == currentHour) {
                 $(this).removeClass("future");
+                $(this).removeClass("past");
                 $(this).addClass("present");
+            }
+            else if (hour > currentHour) {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
             }
         });
 
-    }, 60 * 1000);
+    }, 1000);
 }
 
-
+// Set an event listener to save scheduler input when a save button is clicked
 $(".saveBtn").on("click", function () {
     // Get nearby values of the description in JQuery
     var text = $(this).siblings(".description").val();
@@ -44,4 +75,7 @@ $("#hour15").text(localStorage.getItem("hour15"));
 $("#hour16").text(localStorage.getItem("hour16"));
 $("#hour17").text(localStorage.getItem("hour17"));
 
+// Run the functions that check current time, set the initial time block color and then update the color as time proceeds
+console.log(currentHour);
+initialTimeBlockColor();
 setTimeBlockColor();
